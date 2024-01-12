5eb54113191a5c0dca0271529d8fbd63dbb937e4
Konstantin Makarevich <mak.kostya2029@gmail.com>
2024-01-12 15:35:47 +0300

    fix: rename file

---
+++ dist/TS-Contacts-02.js
@@ -0,0 +1,38 @@
+"use strict";
+const persons = [
+    {
+        name: 'Иван Петров',
+        age: 27,
+        group: 'SEO-специалист',
+    },
+    {
+        name: 'Марат Aляуддинов',
+        age: 20,
+        group: 'Музыкант',
+    },
+    {
+        name: 'Константин Макаревич',
+        age: 23,
+        group: 'Frontend Developer',
+    },
+    {
+        name: 'Кирилл Петров',
+        age: 35,
+        group: 'Переводчик',
+    },
+    {
+        name: 'Иван Иванов',
+        age: 19,
+        group: 'Семья',
+    },
+    {
+        name: 'Сергей Александров',
+        age: 33,
+        role: 'Администратор',
+    },
+];
+const logPerson = (persons) => {
+    console.log(`${persons.name}, ${persons.age}`);
+};
+console.log('Users:');
+persons.forEach(logPerson);
